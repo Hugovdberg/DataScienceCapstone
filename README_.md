748d417f6b1226e0a6f8fb14a7291e1f70736f46
Hugo van den Berg <hugo@tbdwebdesign.nl>
2016-12-01 21:12:00 +0100

    Exploratory analysis

---
+++ README_.md
@@ -0,0 +1,2 @@
+# DataScienceCapstone
+Capstone project for the Data Science Specialisation on Coursera by Johns Hopkins University
---
+++ src/eda.R
@@ -0,0 +1,88 @@
+library('ProjectTemplate')
+reload.project(list(data_loading = FALSE))
+
+# de.Tweets.source <- VectorSource(de.DE.twitter)
+# de.Tweets.corpus <- VCorpus(de.Tweets.source)
+
+## ----- load_data
+corpus.file <- 'data/en.corpus.RData'
+if (!file.exists(corpus.file)) {
+    en.source <- DirSource('data-raw/final/en_US',
+                           pattern = 'en.*\\.txt',
+                           encoding = 'UTF-8')
+    en.corpus <- VCorpus(en.source, readerControl = list(language = 'en'))
+    save(en.corpus, file = corpus.file)
+} else {
+    load(corpus.file)
+}
+
+## ----- summarise_data
+# words <- lapply(en.corpus, function(x)stri_extract_all_words(x$content))
+# en.corpus.summary <- data.frame(
+#     documents = sapply(en.corpus, function(x)length(x$content)),
+#     source_size = sapply(en.corpus, function(x)format(object.size(x), units = 'Mb')),
+#     word_count = sapply(words, function(x)length(unlist(x))),
+#     unique_words = sapply(words, function(x)length(unique(unlist(x)))),
+#     average_words = sapply(words, function(x)mean(sapply(x, length)))
+# )
+# names(en.corpus.summary) <- c('Number of documents', 'Source size',
+#                               'Number of words', 'Number of unique words',
+#                               'Average words per document')
+
+## ----- clean_data
+set.seed(2016-12-01)
+# Subset each source to 20000 lines
+for (s in seq_along(en.corpus)) {
+    en.corpus[[s]]$content %<>% sample(size = min(length(.), 50000))
+}
+
+# define function to remove substrings using regular expressions
+removePattern <- content_transformer(function(x, pattern)gsub(pattern, ' ', x, perl = T))
+
+en.corpus %<>%
+    tm_map(content_transformer(tolower)) %>%
+    # Remove urls
+    tm_map(removePattern, '(https?|ftp)://(\\w+[\\.|/?&=#-]*)+') %>%
+    # Remove mentions, retweets, etc.
+    tm_map(removePattern, '((rt|/?via)( |:)*)?(@\\w+)?(#\\w+)') %>%
+    # tm_map(removeWords, stopwords('en')) %>%
+    tm_map(removeNumbers) %>%
+    tm_map(removePunctuation)
+
+## ----- dtm
+en.dtm <- as.list(seq_along(en.corpus))
+en.freqs <- as.list(seq_along(en.corpus))
+for (k in seq_along(en.corpus)) {
+    en.dtm[[k]] <- DocumentTermMatrix(VCorpus(VectorSource(en.corpus[[k]]$content)))
+    en.dtm[[k]] %<>% removeSparseTerms(.999)
+    en.freqs[[k]] <- sort(colSums(as.matrix(en.dtm[[k]])), decreasing = TRUE)
+}
+names(en.dtm) <- names(en.corpus)
+names(en.freqs) <- names(en.corpus)
+
+# freqs <- data.frame(freq = unlist(en.freqs, use.names = T))
+# freqs %<>%
+#     mutate(
+#         name = row.names(.)
+#     ) %>%
+#     separate(
+#         col = name,
+#         into = c('language', 'source', 'filetype', 'word'),
+#         sep = '\\.'
+#     ) %>%
+#     select(
+#         -filetype
+#     )
+
+## ------------------------ n-grams
+bigram.tokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
+
+en.corpus2 <- en.corpus %>%
+    lapply(function(x)x$content) %>%
+    unlist %>%
+    VectorSource %>%
+    VCorpus
+en.dtm <- en.corpus2 %>%
+    DocumentTermMatrix
+en.dtm.bi <- en.corpus2 %>%
+    DocumentTermMatrix(control = list(tokenizer = bigram.tokenizer))
